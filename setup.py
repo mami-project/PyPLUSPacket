f1b4c32cf9d04ee71c861831f906fd78d6f4deb0
Roman Müntener <me@mroman.ch>
2017-07-10 11:31:53 +0200

    + setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+	name="pluspacket",
+	version="0.1",
+	packages=["pluspacket"],
+
+	author="Roman Müntener",
+	author_email="munt@zhaw.ch",
+	description="Library for parsing PLUS packets.",
+	license="BSD2",
+	keywords="plus parse packet",
+	url="http://github.com/FMNSSun/PyPLUSPacket",
+
+	python_requires=">= 3.5.0"
+)
